0c77356537b55dc73404a46b20d0dab56c6c5216
蒋磊 <835150891@qq.com>
2026-07-27 22:55:47 +0800

    fix: 测试

--- src/database/prisma.service.ts
+++ src/database/prisma.service.ts
@@ -7,6 +7,11 @@ export class PrismaService
   implements OnModuleInit, OnModuleDestroy
 {
   async onModuleInit() {
+    console.log('=== DEBUG PrismaService ===');
+    console.log('NODE_ENV:', process.env.NODE_ENV);
+    console.log('CWD:', process.cwd());
+    console.log('DATABASE_URL:', process.env.DATABASE_URL);
+    console.log('=========================');
     await this.$connect();
   }
 
